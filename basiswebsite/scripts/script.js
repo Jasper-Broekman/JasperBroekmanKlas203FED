ef0ab059a257b7d513f7b7e1150dcbb0c09e88ca
Jasper-Broekman <jasper.broekman@hva.nl>
2025-11-27 10:28:42 +0100

    Add files via upload

---
+++ basiswebsite/scripts/script.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+console.log("hi");
+/*
+const nietzichtbaar = document.getElementById("niet-zichtbaar");
+const header = document.querySelector("header");
+const hamburger = document.getElementById("#hamburger-icon")
+const kruisje = document.getElementById("#sluiten")
+*/
+
+/*
+function toggleMenu() {
+  if (nietzichtbaar.style.display === "flex") {
+    nietzichtbaar.style.display = "none";
+    header.classList.remove("hide-header");
+  } else {
+    nietzichtbaar.style.display = "flex";
+    header.classList.add("hide-header");
+  }
+}
+*/
+
+
+/*
+hamburger.addEventListener ('click', function() {
+    (nietzichtbaar.style.display === "flex"); {
+    nietzichtbaar.style.display = "none";
+    header.classList.remove("hide-header");}
+})
+*/
+
+const nietzichtbaar = document.getElementById("niet-zichtbaar");
+const header = document.querySelector("header");
+const hamburger = document.querySelector("#hamburger-icon");
+const kruisje = document.querySelector("#sluiten");
+
+// MENU OPENEN via hamburger
+hamburger.addEventListener('click', function () {
+    nietzichtbaar.style.display = "flex";
+    header.classList.add("hide-header");
+});
+
+// MENU SLUITEN via kruisje
+kruisje.addEventListener('click', function () {
+    nietzichtbaar.style.display = "none";
+    header.classList.remove("hide-header");
+});
